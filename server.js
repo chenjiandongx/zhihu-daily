cb259ef64c478c860bc99dc96d951ee4a45200c1
chenjiandongx <chenjiandongx@qq.com>
2018-03-21 20:10:08 +0800

    部署准备

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
+const http = require('http')
+const path = require('path')
+const request = require('request')
+const express = require('express')
+const serveStatic = require('serve-static')
 
-const app = express()
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
+const hostname = '127.0.0.1'
+const port = 8010
+const imgPort = 8011
+
+const apiServer = http.createServer((req, res) => {
+  const url = 'http://news-at.zhihu.com/api/4' + req.url
+  const options = {
+    url: url
+  }
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.setHeader('Content-Type', 'text/plain;charset=UTF-8')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.end(body)
+    }
+  })
+}).listen(port, hostname, () => {
+  console.log(`接口代理运行在 http://${hostname}:${port}/`)
 })
 
-const port = process.env.PORT || 5000
-app.listen(port)
-console.log('Server started on port ' + port)
+const imgServer = http.createServer((req, res) => {
+  const url = req.url.split('/img/')[1]
+  const options = {
+    url: url,
+    encoding: null
+  }
+
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const contentType = response.headers['content-type']
+      res.setHeader('Content-Type', contentType)
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.end(body)
+    }
+  })
+}).listen(imgPort, hostname, () => {
+  console.log(`图片代理运行在 http://${hostname}:${imgPort}/`)
+})
+
+
+const app = express();
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+}).listen(process.env.PORT || 5000, ()=> {
+  console.log('项目启动，部署在 5000 端口');
+})
